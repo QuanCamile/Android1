1253e12f4880095743c5cd2df968af98e6484a92
Võ Anh Quân <quancamilee@gmail.com>
2022-11-25 23:55:24 +0700

    Done lesson 6

--- app/src/main/java/com/hutech/exampractice/SignUpActivity.java
+++ app/src/main/java/com/hutech/exampractice/SignUpActivity.java
@@ -1,17 +1,27 @@
 package com.hutech.exampractice;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class SignUpActivity extends AppCompatActivity {
     private EditText name, email, pass, confirmPass;
     private Button signUpB;
     private ImageView backB;
+    private FirebaseAuth mAuth;
+    private String emailStr, passStr, confirmPassStr, nameStr;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +35,8 @@ public class SignUpActivity extends AppCompatActivity {
         signUpB = findViewById(R.id.signupB);
         backB = findViewById(R.id.backB);
 
+        mAuth = FirebaseAuth.getInstance();
+
         backB.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -36,8 +48,68 @@ public class SignUpActivity extends AppCompatActivity {
         signUpB.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                if(validate())
+                {
+                    signUpBNewUser();
+                }
 
             }
         });
+    }
+
+    private boolean validate(){
+        nameStr = name.getText().toString().trim();
+        passStr = pass.getText().toString().trim();
+        emailStr = email.getText().toString().trim();
+        confirmPassStr = confirmPass.getText().toString().trim();
+
+        if(nameStr.isEmpty())
+        {
+            name.setError("Enter Your Name");
+            return false;
+        }
+
+        if(emailStr.isEmpty())
+        {
+            email.setError("Enter Your Email");
+            return false;
+        }
+
+        if(passStr.isEmpty())
+        {
+            pass.setError("Enter Your Password");
+            return false;
+        }
+
+        if(confirmPassStr.isEmpty())
+        {
+            confirmPass.setError("Enter Your Confirm Password");
+            return false;
+        }
+
+        if(passStr.compareTo(confirmPassStr) != 0)
+        {
+            Toast.makeText(SignUpActivity.this, "Password and confirm password should be same!", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+        return true;
+    }
+    private void signUpBNewUser(){
+        mAuth.createUserWithEmailAndPassword(emailStr, passStr)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                           Toast.makeText(SignUpActivity.this, "Sign Up Successful", Toast.LENGTH_SHORT).show();
+                            Intent intent = new Intent(SignUpActivity.this, MainActivity.class);
+                            startActivity(intent);
+                            SignUpActivity.this.finish();
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Toast.makeText(SignUpActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
     }
 }
