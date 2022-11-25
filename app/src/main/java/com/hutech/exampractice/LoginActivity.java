093ebe7a6aebdae700f7d132e219ee93eaf29563
Võ Anh Quân <quancamilee@gmail.com>
2022-11-25 23:26:46 +0700

    Done lesson 5

--- app/src/main/java/com/hutech/exampractice/LoginActivity.java
+++ app/src/main/java/com/hutech/exampractice/LoginActivity.java
@@ -2,6 +2,7 @@ package com.hutech.exampractice;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -9,9 +10,9 @@ import android.widget.EditText;
 import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
-private EditText email, pass;
-private Button loginB;
-private TextView forgotPassB, signupB;
+    private EditText email, pass;
+    private Button loginB;
+    private TextView forgotPassB, signupB;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,28 +24,44 @@ private TextView forgotPassB, signupB;
         loginB = findViewById(R.id.loginB);
         forgotPassB = findViewById(R.id.forgot_password);
         signupB = findViewById(R.id.signupB);
+
         loginB.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if(validateData()){
+                if(validateData())
+                {
                     login();
                 }
             }
         });
+
+        signupB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginActivity.this, SignUpActivity.class);
+                startActivity(intent);
+            }
+        });
     }
+
     private boolean validateData(){
-        boolean status = false;
-        if(email.getText().toString().isEmpty()){
-            email.setError("Enter Email ID");
+
+
+        if(email.getText().toString().isEmpty())
+        {
+            email.setError("Enter E-Mail ID");
             return false;
         }
-        if(pass.getText().toString().isEmpty()){
+
+        if(pass.getText().toString().isEmpty())
+        {
             pass.setError("Enter Password");
             return false;
         }
 
         return true;
     }
+
     private void login(){
 
     }
