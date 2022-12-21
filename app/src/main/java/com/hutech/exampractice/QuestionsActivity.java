1b06c04a89258efb22e81a8c5ad31661a4c3181b
nhatminh-01 <nhatminh1233215@gmail.com>
2022-12-21 21:04:20 +0700

    Done 18

--- app/src/main/java/com/hutech/exampractice/QuestionsActivity.java
+++ app/src/main/java/com/hutech/exampractice/QuestionsActivity.java
@@ -1,14 +1,42 @@
 package com.hutech.exampractice;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class QuestionsActivity extends AppCompatActivity {
 
+    private RecyclerView questionsView;
+    private TextView tvQuesID, timerTV, catNameTV;
+    private Button submitB, markB, clearSelB;
+    private ImageButton prevQuesB, nextQuesB;
+    private ImageView quesListB;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_questions);
+
+
+        init();
+
+        QuestionsAdapter questionsAdapter = new QuestionsAdapter(DbQuery.g_quesList);
+        questionsView.setAdapter(questionsAdapter);
+    }
+    private void init(){
+        questionsView = findViewById(R.id.questions_view);
+        tvQuesID = findViewById(R.id.tv_quesID);
+        timerTV = findViewById(R.id.tv_timer);
+        catNameTV = findViewById(R.id.qa_catName);
+        submitB = findViewById(R.id.submitB);
+        markB = findViewById(R.id.markB);
+        clearSelB = findViewById(R.id.clear_selB);
+        prevQuesB = findViewById(R.id.prev_quesB);
+        nextQuesB = findViewById(R.id.next_quesB);
+        quesListB = findViewById(R.id.ques_list_gridB);
     }
 }
